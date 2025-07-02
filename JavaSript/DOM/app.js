a4d2d22eeecdb830a188c34c22c0906170302779
deepikapathak25 <pathakdeepika43@gmail.com>
2025-07-02 14:18:10 +0530

    DOM practice

---
+++ JavaSript/DOM/app.js
@@ -0,0 +1,7 @@
+console.dir(document.querySelectorAll("p"));
+
+// console.dir(document.querySelector("#description"));
+
+// console.dir(document.querySelector(".oldImg"));
+
+console.dir(document.querySelectorAll(".box a"));
